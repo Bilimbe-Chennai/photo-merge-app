3926867c31bda335ff554d01c22fe59d31b74a55
Dhivyapriya9933 <dhivyapriya0303@gmail.com>
2025-12-29 20:20:16 +0530

    first version build success

---
+++ android/app/src/main/assets/templates.js
@@ -0,0 +1,10 @@
+export const TEMPLATES = [
+    //{ id: 'template1.png', src: require('../assets/template1.png') },
+  { id: 'template14.png', src: require('../assets/template14.png') },
+  { id: 'template15.png', src: require('../assets/template15.png') },
+  { id: 'template16.png', src: require('../assets/template16.png') },
+  { id: 'template17.png', src: require('../assets/template17.png') },
+  { id: 'template18.png', src: require('../assets/template18.png') },
+  { id: 'template12.png', src: require('../assets/template12.png') },
+  { id: 'template13.png', src: require('../assets/template13.png') },
+];
